c608c1923ea8a3f7d6395fd90db9f6737e764b46
Manon <manon.graphics.witch@gmail.com>
2025-01-25 14:49:25 +0100

    Adjust player size and position, and camera position.

--- kinect-bubbles/kinect_player.gd
+++ kinect-bubbles/kinect_player.gd
@@ -65,15 +65,33 @@ func _ready() -> void:
 			joint.name = "Joint" + str(joint_id)
 			add_child(joint)
 
+		var plane_mesh = PlaneMesh.new()
+		var plane = MeshInstance3D.new()
+		plane.name = "Ground"
+		plane.mesh = plane_mesh
+		plane.set_position(Vector3(1, 1, 1))
+		add_child(plane)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+
 	if self.kinect_node != null:
 		var body = self.kinect_node.get_body(0) as KinectBody
 
+		var plane_direction = Vector3(
+			body.ground_plane.x, body.ground_plane.y, body.ground_plane.z);
+		var plane_offset = -plane_direction * body.ground_plane.w
+
+		var ground = self.get_node("Ground");
+		ground.set_position(plane_offset)
+		ground.look_at(plane_offset + plane_direction, Vector3.UP)
+
 		for joint_id in range(KinectBody.JointID_Count):
-			var kin_pos = body.get_joint_position(joint_id)
+			var kin_pos = body.get_joint_position(joint_id) - plane_offset
+			if joint_id == KinectBody.JointID_FootLeft:
+				print(kin_pos)
 			var scene_pos = kin_pos * player_size
+			scene_pos.z += 22.0
 			var joint = self.get_node("Joint" + str(joint_id))
 			joint.set_position(scene_pos)
-
