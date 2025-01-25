b1918ee821854b17b6eb2c96191aba1c9a36266f
Manon <manon.graphics.witch@gmail.com>
2025-01-25 21:34:37 +0100

    Respawn limb after touching a bubble.

--- kinect-bubbles/kinect_body_node/kinect_body_node.gd
+++ kinect-bubbles/kinect_body_node/kinect_body_node.gd
@@ -9,14 +9,23 @@ func _on_body_entered(body: Node) -> void:
 		return;
 	
 	if body.is_in_group("bubble"):
-			body.pop()
+		body.pop()
+		var kinect_player = self.get_parent()
+		if kinect_player.name == "KinectPlayer": # WTF Godot
+			kinect_player.restore_limb()
 
 	if body.is_in_group("spiky_object"):
 		body.touched_spike()
 		var kinect_player = self.get_parent()
-		kinect_player.pop_limb(self.joint_id)
+		if kinect_player.name == "KinectPlayer": # WTF Godot
+			kinect_player.pop_limb(self.joint_id)
 
 func pop() -> void:
 	disabled = true
 	$CollisionShape3D.disabled = true # Doesn't seem to do anything?
 	$AnimatedSprite3D.play("pop")
+
+func spawn() -> void:
+	$AnimatedSprite3D.play("default")
+	disabled = false
+	$CollisionShape3D.disabled = false # Doesn't seem to do anything?
