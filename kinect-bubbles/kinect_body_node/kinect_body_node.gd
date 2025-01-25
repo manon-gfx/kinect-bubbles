b1d14b708a390aa119fd3f1e72ad439eb2968aad
Manon <manon.graphics.witch@gmail.com>
2025-01-25 18:37:58 +0100

    Improve the player bubble visuals.

--- kinect-bubbles/kinect_body_node/kinect_body_node.gd
+++ kinect-bubbles/kinect_body_node/kinect_body_node.gd
@@ -5,7 +5,7 @@ extends Area3D
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("bubble"):
 			body.pop()
-			
+
 	if body.is_in_group("spiky_object"):
 		print("Spiky object!!")
 		body.touched_spike()
