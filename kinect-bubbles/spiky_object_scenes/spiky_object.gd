879cfe07fb7cfb4e2a6014d6b93fe9e6b29eb9ad
Anne Poot <annepoot96@gmail.com>
2025-01-25 20:23:07 +0100

    Made spikes move (within frame)

--- kinect-bubbles/spiky_object_scenes/spiky_object.gd
+++ kinect-bubbles/spiky_object_scenes/spiky_object.gd
@@ -3,19 +3,30 @@ extends CharacterBody3D
 # emit when the player pops a bubble
 signal spiked(name)
 
-var rotation_axis = Vector3(0, 0, 1) 
-var rotation_amount = 0.1
-var total_rotation = 0.0
 var camera_pos = 0
 var spike_name = "default"
 
+var rng = RandomNumberGenerator.new()
+var direction = Vector3(rng.randfn(0.0, 1.0), rng.randfn(0.0, 1.0), 0.0).normalized()
+var speed = 2.0
 
 func touched_spike():
 	print("touched spike: ", spike_name, "!")
 	spiked.emit(name)
 	
-
+	
 func initialize(start_position: Vector3, scale_factor: float, _spike_name):
 	spike_name = _spike_name
 	position = start_position
 	scale = Vector3(scale_factor, scale_factor, scale_factor)
+
+
+func _process(delta: float) -> void:
+	if position.x < -40 or position.x > 40:
+		direction.x = -direction.x
+	if position.y < 0 or position.y > 40: 
+		direction.y = -direction.y
+
+	var dv = speed * direction * delta
+	velocity += dv
+	move_and_collide(dv)
