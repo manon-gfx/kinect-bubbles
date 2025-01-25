4ce5dba04873a3dce7f0f4fd132a47479a3e77c8
Anne Poot <annepoot96@gmail.com>
2025-01-25 18:36:57 +0100

    Updated bubble.gd

--- kinect-bubbles/bubble_scene/bubble.gd
+++ kinect-bubbles/bubble_scene/bubble.gd
@@ -12,38 +12,37 @@ func pop():
 	$AnimatedSprite3D.play("pop")
 	popped.emit(name)
 
-	
-	
-#func _ready() -> void:
-	#pass
-
-var rotation_axis = Vector3(0, 0, 1) 
-var rotation_amount = 0.1
-var total_rotation = 0.0
 var camera_pos = 0
+var radius = 1
 var bubble_name = "default"
+
+var acceleration = Vector3.ZERO
+var drift = 50
+var stiffness = 0.1
+var rng = RandomNumberGenerator.new()
+
+var target = Vector3.ZERO
 
 # rotate sprite by certain amount of radians (probably) while it keeps looking at the camera
 func initialize(start_position: Vector3, scale_factor: float, _bubble_name) -> void:
 	position = start_position
 	bubble_name = _bubble_name
 	scale = Vector3(scale_factor, scale_factor, scale_factor)
-	#print(bubble_name)
-	
-	# defer because camera doesnt exist yet (might not be necessary, this is a manual billboarding)
-	#call_deferred("get_camera_pos_for_init",start_position, _rotation_amount)
+	target = position
 
-func get_camera_pos_for_init(start_position: Vector3, _rotation_amount: float) -> void:
+func _process(delta: float) -> void:
+	target += drift * Vector3(rng.randfn(0.0, 1.0), rng.randfn(0.0, 1.0), 0.0) * delta
+	
+	var a = stiffness * (target - position)
+	var dv = delta * a
+	velocity += dv
+	move_and_collide(dv)
+
+func get_camera_pos_for_init(start_position: Vector3, radius: float) -> void:
 	camera_pos = get_viewport().get_camera_3d().global_transform.origin
 	camera_pos.y = global_position.y
 	look_at_from_position(start_position, camera_pos, Vector3.UP)
-	
-# rotate sprite by certain amount of radians (probably) while it keeps looking at the camera
-func rotate_sprite(rotation_amount: float):
-	camera_pos = get_viewport().get_camera_3d().global_transform.origin
-	camera_pos.y = global_position.y
-	look_at(camera_pos, Vector3(0, 1, 0).rotated(rotation_axis, total_rotation))
-	
+
 # remove the node from memory after the animation has finished playing
 func _on_animated_sprite_3d_animation_finished() -> void:
 	queue_free()
