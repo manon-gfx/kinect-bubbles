74c08aaf3b8c8a9f1e8f22259bbef596f8fde273
Manon <manon.graphics.witch@gmail.com>
2025-01-25 20:54:59 +0100

    Pop limb when hitting a spiky object.

--- kinect-bubbles/kinect_body_node/kinect_body_node.gd
+++ kinect-bubbles/kinect_body_node/kinect_body_node.gd
@@ -1,12 +1,22 @@
 extends Area3D
 
-
+var joint_id = 0
+var disabled = false
 
 func _on_body_entered(body: Node) -> void:
+	# Skip collisions if disabled
+	if disabled:
+		return;
+	
 	if body.is_in_group("bubble"):
 			body.pop()
 
 	if body.is_in_group("spiky_object"):
-		print("Spiky object!!")
 		body.touched_spike()
-		$AnimatedSprite3D.play("pop")
+		var kinect_player = self.get_parent()
+		kinect_player.pop_limb(self.joint_id)
+
+func pop() -> void:
+	disabled = true
+	$CollisionShape3D.disabled = true # Doesn't seem to do anything?
+	$AnimatedSprite3D.play("pop")
