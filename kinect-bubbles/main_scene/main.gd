abcfc6b7c56bc7cd481d81a2cbee8d46314a1117
Manon <manon.graphics.witch@gmail.com>
2025-01-26 13:13:47 +0100

    Tweak how spikies work in the game.

--- kinect-bubbles/main_scene/main.gd
+++ kinect-bubbles/main_scene/main.gd
@@ -14,8 +14,42 @@ var pint_scene = preload("res://spiky_object_scenes/pint.tscn")
 var pint_2_scene = preload("res://spiky_object_scenes/pint_2.tscn")
 var thorn_scene = preload("res://spiky_object_scenes/thorn.tscn")
 
-var list_of_spiky_objects = [broken_bottle_scene, cactus_01_scene, cactus_02_scene, cactus_03_scene, knife_scene, nail_in_wood_scene,
-spike_scene, pint_scene, pint_2_scene, thorn_scene]
+const BROKEN_BOTTLE = 0
+const CACTUS1= 1
+const CACTUS2 = 2
+const CACTUS3 = 3
+const KNIFE = 4
+const NAIL_IN_WOOD = 5
+const SPIKE = 6
+const PINT = 7
+const PINT2 = 8
+#const THORN = 9
+const SPIKY_COUNT = 9
+
+const SPIKY_CATEGORY_PIN = 0
+const SPIKY_CATEGORY_CACTUS = 1
+const SPIKY_CATEGORY_OTHER = 2
+
+func spiky_type_to_cat(spiky_type):
+	if spiky_type == PINT || spiky_type == PINT2 || spiky_type == KNIFE:
+		return SPIKY_CATEGORY_PIN
+	elif spiky_type == CACTUS1 || spiky_type == CACTUS2 || spiky_type == CACTUS3:
+		return SPIKY_CATEGORY_CACTUS
+	else:
+		return SPIKY_CATEGORY_OTHER
+
+var list_of_spiky_objects = [
+	broken_bottle_scene,
+	cactus_01_scene,
+	cactus_02_scene,
+	cactus_03_scene,
+	knife_scene,
+	nail_in_wood_scene,
+	spike_scene,
+	pint_scene,
+	pint_2_scene,
+	#thorn_scene
+]
 var min_x
 var max_x
 var min_y
@@ -44,10 +78,9 @@ func _unhandled_input(event):
 				esc_pressed_once = true
 		elif event.keycode != KEY_ESCAPE:
 			esc_pressed_once = false
-			
-		
+
 var rng: RandomNumberGenerator
-	
+
 func _ready() -> void:
 	var shape_box = $BubbleArea/CollisionShape3D.shape.size
 	var pos_box = $BubbleArea/CollisionShape3D.position
@@ -57,26 +90,36 @@ func _ready() -> void:
 	max_y = pos_box[1]+shape_box[1] - bubble_radius
 	min_z = pos_box[2]-shape_box[2] + bubble_radius
 	max_z = pos_box[2]+shape_box[2] - bubble_radius
-	
+
+	const MAGIC_Z = 32.596; # emperically found
+	var min_bounds = $Camera3D.project_position(Vector2.ZERO, MAGIC_Z)
+	var max_bounds = $Camera3D.project_position($Camera3D.get_viewport().size, MAGIC_Z)
+	min_x = min_bounds.x
+	max_x = max_bounds.x
+	min_y = max_bounds.y # note these are swapped, not a bug
+	max_y = min_bounds.y
+
+	print(min_y)
+	print(max_y)
+
+
 	rng = RandomNumberGenerator.new()
 
 	for i in range(number_of_bubbles):
 		add_bubble_in_random_loc(rng)
-	
-	for i in range(number_of_spikes):
-		add_spike_in_random_loc(rng)
+#
+	#for i in range(number_of_spikes):
+		#add_spike_in_random_loc(rng)
 	if not $KinectPlayer.has_a_kinect:
 		$player.queue_free()
 
 func calc_spawn_location(rng):
-	match rng.randi() % 4:
-		0:
+	match rng.randi() % 3:
+		0: # Left
 			spawn_location = Vector3(min_x - offset, rng.randf_range(min_y, max_y), Z_COORD_COLLIDERS)
-		1:
+		1: # Right
 			spawn_location = Vector3(max_x + offset, rng.randf_range(min_y, max_y), Z_COORD_COLLIDERS)
-		2:
-			spawn_location = Vector3(rng.randf_range(min_x, max_x), min_y - offset, Z_COORD_COLLIDERS)
-		3:
+		2: # Top
 			spawn_location = Vector3(rng.randf_range(min_x, max_x), max_y + offset, Z_COORD_COLLIDERS)
 
 	return spawn_location
@@ -91,23 +134,63 @@ func add_bubble_in_random_loc(rng):
 	bubble.popped.connect($UserInterface._on_bubble_popped)
 
 func add_spike_in_random_loc(rng):
-	
-	
-	var spiky = list_of_spiky_objects.pick_random().instantiate()
-	spiky.initialize(calc_spawn_location(rng), rng.randf_range(0.2,0.6), rng.randi())
-	
+	var spiky_type = rng.randi_range(0, SPIKY_COUNT - 1)
+	var spiky_cat = spiky_type_to_cat(spiky_type)
+	var spiky = list_of_spiky_objects[spiky_type].instantiate()
 
+	var spawn_location = Vector3.ZERO # invalid
+	var spawn_direction = Vector3.ZERO
+
+	var center_x = (min_x + max_x) * 0.5
+	var center_y = (min_y + max_y) * 0.5
+	var center = Vector3(center_x, center_y, Z_COORD_COLLIDERS);
+
+
+	var side = rng.randi() % 3
+	var target = center + Vector3(rng.randf_range(-15.0, 15.0), -4.0, 0.0)
+	match side:
+		0: # Left
+			spawn_location = Vector3(min_x - offset, rng.randf_range(min_y + 10.0, max_y), Z_COORD_COLLIDERS)
+			target = center + Vector3(rng.randf_range(-15.0, 0.0), -4.0, 0.0)
+		1: # Right
+			spawn_location = Vector3(max_x + offset, rng.randf_range(min_y + 10.0, max_y), Z_COORD_COLLIDERS)
+			target = center + Vector3(rng.randf_range(0.0, 15.0), -4.0, 0.0)
+		2: # Top
+			spawn_location = Vector3(rng.randf_range(min_x, max_x), max_y + offset, Z_COORD_COLLIDERS)
+
+	print("center:" + str(center) + " Spawn Location: " + str(spawn_location))
+
+	spawn_direction = (target - spawn_location).normalized()
+
+	spiky.initialize(
+		spawn_location,
+		#calc_spawn_location(rng),
+		0.2,
+		rng.randi()
+	)
+	spiky.direction = spawn_direction
+
+	if spiky_cat == SPIKY_CATEGORY_PIN:
+		spiky.speed = 4.0
+		spiky.rotating = false
+	else:
+		spiky.speed = 2.0
+		var rotspd = rng.randf_range(3.0, 5.0)
+		if rng.randi() % 2 == 0:
+			rotspd = -rotspd
+		spiky.rot_speed = rotspd
+
+	#spiky.speed = 0.0
 
 	# Spawn the bubble by adding it to the Main scene.
 	add_child(spiky)
 	spiky.spiked.connect($UserInterface._on_spike_touched)
-	
 
-func _on_bubble_and_spike_timer_timeout() -> void:
+func _on_spike_timer_timeout() -> void:
+	$SpikeTimer.wait_time = rng.randf_range(4.0, 6.0)
+	add_spike_in_random_loc(rng)
+
+func _on_bubble_timer_timeout() -> void:
 	#$BubbleAndSpikeTimer.wait_time = rng.randf_range(1,6)
-	$BubbleAndSpikeTimer.wait_time = rng.randf_range(0,1)
-	if rng.randf() > 0.8:
-		add_spike_in_random_loc(rng)
-	else:
-		add_bubble_in_random_loc(rng)
-	
+	$BubbleTimer.wait_time = rng.randf_range(1.0, 2.0)
+	add_bubble_in_random_loc(rng)
--- kinect-bubbles/spiky_object_scenes/spiky_object.gd
+++ kinect-bubbles/spiky_object_scenes/spiky_object.gd
@@ -7,31 +7,43 @@ var camera_pos = 0
 var spike_name = "default"
 
 var rng = RandomNumberGenerator.new()
-var direction = Vector3(rng.randfn(0.0, 1.0), rng.randfn(0.0, 1.0), 0.0).normalized()
+var direction = Vector3.UP
 var speed = 2.0
+var rotating = true
+var rot_speed = 2.0
+
+var current_rotation = 0.0
 
 func touched_spike():
-	print("touched spike: ", spike_name, "!")
 	spiked.emit(name)
-	
-	
-func initialize(start_position: Vector3, scale_factor: float, _spike_name):
+
+
+func initialize(
+	start_position: Vector3,
+	scale_factor: float,
+	_spike_name):
 	spike_name = _spike_name
 	position = start_position
 	scale = Vector3(scale_factor, scale_factor, scale_factor)
 	collision_mask = 1
 
+func _process(delta: float) -> void:
+	# if can_bounce:
+	# 	if position.x < -40 or position.x > 40:
+	# 		direction.x = -direction.x
+	# 	if position.y < 0 or position.y > 40:
+	# 		direction.y = -direction.y`
+	if rotating:
+		current_rotation += delta * rot_speed
+	else:
+		current_rotation = atan2(direction.y, direction.x) - (PI * 0.5)
+	set_rotation(Vector3(0.0, 0.0, current_rotation))
 
 func _physics_process(delta: float) -> void:
-	if position.x < -40 or position.x > 40:
-		direction.x = -direction.x
-	if position.y < 0 or position.y > 40: 
-		direction.y = -direction.y
-
 	var dv = speed * direction * delta
 	velocity += dv
 	var collision = move_and_collide(dv)
-	
+
 	if collision:
 		var collider = collision.get_collider()
 		if collider.is_in_group("bubble"):
